fcb715f4ea9590b873b3cd01416bbd874aaa2c64
Renata Souza <renataapsouza@outlook.com>
2024-07-08 10:11:53 -0300

    Inicio projeto Spring

---
+++ src/main/java/br/com/alura/sreenmatch/SreenmatchApplication.java
@@ -0,0 +1,27 @@
+package br.com.alura.sreenmatch;
+
+import br.com.alura.model.DadosSerie;
+import br.com.alura.service.ConsumoApi;
+import br.com.alura.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoApi consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=a92f802");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+	}
+}
